670c3a049af4be613ea47feb00eedf585f57b8ba
Julissa749 <salinasjulissa6@gmail.com>
2020-05-14 13:23:41 -0600

    este es mi segundo commit

---
+++ index2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+$Base=15;
+
+$Altura=12;
+
+$Area = ($Base * $Altura)/2;
+
+printf (“El area del triangulo es: $Area”);
+
+?>
+</body>
+</html>
