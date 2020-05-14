ff7d87ab98296caff3c02bc1e497696d8c65a5b4
Julissa749 <salinasjulissa6@gmail.com>
2020-05-14 13:40:50 -0600

    este es mi tecer commit

---
+++ index3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+      <?
+      $numero1=15; $numero2=30;
+      $mayor=0;
+
+     if ($numero1 > $numero2)
+        $mayor=$numero1;
+
+     else
+      $mayor=$numero2;
+       echo “Numero 1= $numero1 <BR>”;
+      echo “Numero 2= $numero2 <BR>”; 
+
+      echo “El mayor es $mayor”;
+     ?>
+
+</body>
+</html>
